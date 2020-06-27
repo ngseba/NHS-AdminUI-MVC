b991316d0b3ac9702b503809ccb09e9b48d4390f
Seba Neagrau <seba.ng@yahoo.com>
2020-06-27 22:09:04 +0300

    localhost to remote

--- src/main/java/ro/iteahome/nhs/adminui/service/DoctorService.java
+++ src/main/java/ro/iteahome/nhs/adminui/service/DoctorService.java
@@ -24,8 +24,8 @@ public class DoctorService {
 
     private final String CREDENTIALS = "NHS_ADMIN_UI:P@ssW0rd!";
     private final String ENCODED_CREDENTIALS = new String(Base64.getEncoder().encode(CREDENTIALS.getBytes()));
-    //private final String DOCTORS_URL = "http://nhsbackendstage.myserverapps.com/doctors";
-    private final String DOCTORS_URL = "http://localhost:8081/doctors";
+    private final String DOCTORS_URL = "http://nhsbackendstage.myserverapps.com/doctors";
+    //private final String DOCTORS_URL = "http://localhost:8081/doctors";
 
 // AUTHENTICATION FOR REST REQUESTS: -----------------------------------------------------------------------------------
 
