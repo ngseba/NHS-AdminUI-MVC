39617971b7505c7e3abbded2bb5cde3df9a76fb8
Mihai Szocs <szocsmihai@gmail.com>
2020-07-01 17:19:34 +0300

    Fixed missing curly brace.

--- src/main/java/ro/iteahome/nhs/adminui/config/RestUrlConfig.java
+++ src/main/java/ro/iteahome/nhs/adminui/config/RestUrlConfig.java
@@ -4,4 +4,4 @@ public class RestUrlConfig {
 
     public static final String SERVER_ROOT_URL = "https://nhsbackend.myserverapps.com";
     //public static final String SERVER_ROOT_URL = "http://localhost:8081";
-
+}
